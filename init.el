a906fd27cf10caabb2223c36e677336f2a49801f
orehathiya <merrynewyear6593@gmail.com>
2015-09-03 01:08:48 +0900

    first commit

---
+++ init.el
@@ -0,0 +1,5 @@
+(global-set-key "\C-h" 'delete-backward-char)
+
+(setq scheme-program-name "gosh -i")
+(autoload 'scheme-mode "cmuscheme" "Major mode for Scheme." t)
+(autoload 'run-scheme "cmuscheme" "Run an inferior Scheme process." t)
